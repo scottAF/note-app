a3bae622daf71781fed90fd1b0d7b85c75d8e2c1
Scott Ford <uofoduck14@gmail.com>
2019-09-03 21:00:19 +0000

    added constructor to app, renders note or list.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import List from './components/List';
 import Note from './components/Note';
 
-function App() {
-  
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showNote: false
+    };
+  }
+
+  render() {
+    const { showNote } = this.state;
+
     return (
       <div className="App">
         <Nav />
-        <List />
-        <Note />
+        { showNote ? <List /> : <Note /> }
       </div>
     );
   }
+}
 
 
 export default App;
