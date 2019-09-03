8899071bfdbf27e12d108503b721c8e987c23bf2
Scott Ford <uofoduck14@gmail.com>
2019-09-03 20:44:10 +0000

    added the correct css to applicaiton and created the Note and List component.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import List from './components/List';
+import Note from './components/Note';
 
 function App() {
   
     return (
       <div className="App">
         <Nav />
+        <List />
+        <Note />
       </div>
     );
   }
