9f54fdb379c66185f6808c64941c0c7c199612ec
Scott Ford <uofoduck14@gmail.com>
2019-09-16 18:56:10 +0000

    added google icons to notecards.

--- src/components/NoteCard.js
+++ src/components/NoteCard.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class NoteCard extends React.Component {
+class NoteCard extends Component {
   render() {
+    const { note } = this.props;
     return (
-      <div className="note-card-cotainer">
-        NotesCard Component
+      <div className="note-card-container">
+        <div className="note-card-title">
+          {note.title}
+        </div>
+        <div className="note-card-content" >
+          {note.content}
+        </div>
+        <span className="note-card-delete">
+          <i className="material-icons">close</i>
+        </span>
+        <span className="note-card-edit">
+          <i className="material-icons">mode_edit</i>
+        </span>
       </div>
     );
   }
