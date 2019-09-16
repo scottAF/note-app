f300199923ae964c30b9825a2339f5a79fb8d6ac
Scott Ford <uofoduck14@gmail.com>
2019-09-16 16:44:24 +0000

    added NoteCard subcomponents to List.

---
+++ src/components/NoteCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NoteCard extends React.Component {
+  render() {
+    return (
+      <div className="note-card-cotainer">
+        NotesCard Component
+      </div>
+    );
+  }
+}
+
+export default NoteCard;
