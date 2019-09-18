8fc6052c5b24164d806dd3bbaa350236a73e570d
Scott Ford <uofoduck14@gmail.com>
2019-09-18 15:29:22 +0000

    added click method to app to allow note icons interaction.

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ class App extends Component {
     .catch((err) => console.log(err.response.data));
   }
 
+  getNote = () => {
+    console.log('Clicked');
+  }
+
   render() {
     const { showNote, notes } = this.state; // object destructuring
 
@@ -36,7 +40,11 @@ class App extends Component {
         { showNote ? 
           <Note /> 
           : 
-          <List getNotes={this.getNotes} notes={notes}/> 
+          <List 
+            getNotes={this.getNotes} 
+            notes={notes} 
+            getNote={this.getNote} 
+          /> 
         }
       </div>
     );
